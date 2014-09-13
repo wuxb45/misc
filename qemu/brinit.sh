45b13fe06fe627947905edab44b2b2482bd4e17f
Wu Xingbo <wuxb45@gmail.com>
2014-09-13 15:17:45 -0400

    update brinit

--- qemu/brinit.sh
+++ qemu/brinit.sh
@@ -4,34 +4,44 @@ if [[ $(whoami) != 'root' ]]; then
   exit 0
 fi
 
-fail=
 check_cmd ()
 {
   if [[ -z $(which $1) ]]; then
     echo No $1
-    fail=1
+    exit 0
   fi
 }
-
-#### set allow br0 in /etc/qemu/bridge.conf
-qbrconf="/etc/qemu/bridge.conf"
-if [[ ! -e ${qbrconf} ]]; then
-  echo "==> ${qbrconf} Set allow br0"
-  echo 'allow br0' > ${qbrconf}
-elif [[ -z $(grep '^allow br0$' ${qbrconf}) ]]; then
-  echo "==> ${qbrconf} Add allow br0"
-  echo 'allow br0' >> ${qbrconf}
-else
-  echo "==| found ${qbrconf} allow br0"
-fi
 
 check_cmd brctl
 check_cmd sysctl
 check_cmd iptables
 
-if [[ $fail ]]; then
+outport=$1
+if [[ -z ${outport} ]]; then
+  echo "usage: $0 <out-port>"
   exit 0
 fi
+
+ip link show dev ${outport} &> /dev/null
+if [ $? != 0 ]; then
+  echo "==! Out port not found"
+  exit 0
+fi
+
+brname="nat@${outport}"
+
+#### set allow br in /etc/qemu/bridge.conf
+qbrconf="/etc/qemu/bridge.conf"
+if [[ ! -e ${qbrconf} ]]; then
+  echo "==> ${qbrconf} Set allow ${brname}"
+  echo "allow ${brname}" > ${qbrconf}
+elif [[ -z $(grep "^allow ${brname}"'$' ${qbrconf}) ]]; then
+  echo "==> ${qbrconf} Add allow ${brname}"
+  echo "allow ${brname}" >> ${qbrconf}
+else
+  echo "==| found ${qbrconf} allow ${brname}"
+fi
+
 
 if [[ ! $(sysctl net.ipv4.ip_forward | egrep '.*=.*1$') ]]; then
   echo "==> set ip_forward = 1"
@@ -40,32 +50,22 @@ else
   echo "==| found ip_forward = 1"
 fi
 
-if [[ -z $(brctl show | grep '^br0') ]]; then
-  echo "==> add br0"
-  brctl addbr br0
+if [[ -z $(brctl show | grep "^${brname}") ]]; then
+  echo "==> add ${brname}"
+  brctl addbr "${brname}"
 else
-  echo "==| found br0"
+  echo "==| found ${brname}"
 fi
 
-if [[ -z $(ip addr show dev br0 | grep '10\.0\.0\.1/8') ]]; then
-  echo "==> assign 10.0.0.1/8 to br0"
-  ip addr add 10.0.0.1/8 dev br0
-  ip link set dev br0 up
+if [[ -z $(ip addr show dev ${brname} | grep '10\.0\.0\.1/8') ]]; then
+  echo "==> assign 10.0.0.1/8 to ${brname}"
+  ip addr add 10.0.0.1/8 dev ${brname}
+  ip link set dev ${brname} up
 else
-  echo "==| found br0 ip"
+  echo "==| found ${brname} ip"
 fi
 
-outport=$1
-if [ -z ${outport} ]; then
-  exit 0
-fi
-
-ip link show dev ${outport} &> /dev/null
-if [ $? != 0 ]; then
-  echo "==! Out port not found, skip NAT"
-  exit 0
-fi
-echo NAT to ${outport}
+echo "configure NAT to ${outport}"
 
 iptables_put_rule()
 {
@@ -78,12 +78,12 @@ iptables_put_rule()
   fi
 }
 
-# allow br0
+# allow br
 iptables_put_rule FORWARD -m physdev --physdev-is-bridged -j ACCEPT
 
 # allow nat in/out
-iptables_put_rule FORWARD -i ${outport} -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables_put_rule FORWARD -i br0 -o ${outport} -j ACCEPT
+iptables_put_rule FORWARD -i ${outport} -o ${brname} -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables_put_rule FORWARD -i ${brname} -o ${outport} -j ACCEPT
 
 # set NAT
 natrule="POSTROUTING -o ${outport} -j MASQUERADE"
