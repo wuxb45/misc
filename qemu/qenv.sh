76e5561cf5ebe802e963680f5c3324f4ea40b01d
Wu Xingbo <wuxb45@gmail.com>
2014-10-30 23:22:43 -0400

    most rewrite qenv.sh

--- qemu/qenv.sh
+++ qemu/qenv.sh
@@ -1,43 +1,73 @@
 #!/bin/bash
+if [[ -e "${1}" ]]; then
+  . ${1}
+fi
 
-# $1: nr_cpus
-# $2: memory size
+# local_id=0-n
+# display=none
+gen_display()
+{
+  local _local_id=${local_id:-0}
+  if [[ ${display} == "none" ]]; then
+    echo "-vga none -display none "
+  else
+    echo "-vga vmware -display vnc=0.0.0.0:${_local_id} "
+  fi
+}
+
+# nr_cpus=1-n
+# mem_size=1M,1G
 gen_cpu_memory()
 {
-  echo "qemu-system-x86_64 -smp ${1} -m ${2} \
-  -vga none -display none \
-  -cpu host -machine accel=kvm -enable-kvm -daemonize "
+  local _nr_cpus=${nr_cpus:-1}
+  local _mem_size=${mem_size:-256M}
+  echo "-smp ${_nr_cpus} -m ${_mem_size} -cpu host -machine accel=kvm -enable-kvm -daemonize "
 }
 
-# $1: port number: it will be added by 10000
+# local_id=0-n
 gen_serial()
 {
+  local _local_id=${local_id:-0}
   ## you need to add "console=ttyS0" to guest's kernel parameters
-  echo "-serial telnet:localhost:$((10000 + $1)),server,nowait"
+  echo "-serial telnet:localhost:$((10000 + ${_local_id})),server,nowait"
 }
 
-# $1: port number: it will be added by 20000
+# local_id=0-n
 gen_monitor()
 {
-  ## you need to add "console=ttyS0" to guest's kernel parameters
-  echo "-monitor telnet:localhost:$((20000 + $1)),server,nowait"
+  local _local_id=${local_id:-0}
+  echo "-monitor telnet:localhost:$((20000 + ${_local_id})),server,nowait"
 }
 
-# $1: brname
-# $2: mac suffix, at most 65535
+# bridge=br0
+# mac3=00-ff
+# mac4=00-ff
+# mac5=00-ff
+# mac6=00-ff
 gen_bridge()
 {
-  local mac0=${MAC0:-00}
-  local mac1=$(printf '%02x' $(($2 / 256)) )
-  local mac2=$(printf '%02x' $(($2 % 256)) )
-  echo "-net bridge,br=${1} -net nic,model=virtio,macaddr=52:54:00:${mac0}:${mac1}:${mac2}"
+  if [[ -n ${bridge} ]]; then
+    local _mac3=${mac3:-00}
+    local _mac4=${mac4:-00}
+    local _mac5=${mac5:-00}
+    local _mac6=${mac6:-00}
+    echo "-net bridge,br=${bridge} -net nic,model=virtio,macaddr=52:54:${_mac3}:${_mac4}:${_mac5}:${_mac6} "
+  fi
 }
 
-# $1 image file
-# $2 image format
-gen_disk()
+# main_img=
+# main_fmt=
+gen_disks()
 {
-  echo "-drive file=${1},if=virtio,aio=native,discard=on,format=${2}"
+  for i in $(seq 0 4); do
+    if [[ -n ${base_img[${i}]} && -n ${base_fmt[${i}]} ]]; then
+      if [[ -n ${cow_img[${i}]} && -n ${cow_fmt[${i}]} ]]; then
+        echo "-drive file=${cow_img[${i}]},if=virtio,aio=native,discard=on,format=${cow_fmt[${i}]} "
+      else
+        echo "-drive file=${base_img[${i}]},if=virtio,aio=native,discard=on,format=${base_fmt[${i}]} "
+      fi
+    fi
+  done
 }
 
 # $1 new image filename
@@ -46,40 +76,39 @@ gen_disk()
 # $4 base image format
 touch_cow()
 {
-  if [[ $# < 4 ]]; then
-    echo "Usage: $0 <image> <format> <backing-image> <backing-format>"
-    return 1
-  fi
-  if [[ ! -e "$1" ]]; then
-    qemu-img create -f "$2" -o backing_file="$3",backing_fmt="$4" "$1"
-    return $!
-  else
-    echo "found cow image $1"
-    qemu-img info "$1"
-    return $!
-  fi
+  for i in $(seq 0 4); do
+    if [[ -n ${base_img[${i}]} && -n ${base_fmt[${i}]} && -n ${cow_img[${i}]} && -n ${cow_fmt[${i}]} ]]; then
+      if [[ ! -e "${base_img[${i}]}" ]]; then
+        echo "[touch_cow] base_img ${base_img[${i}]} not found!"
+        exit 1
+      fi
+      if [[ ! -e "${cow_img[${i}]}" ]]; then
+        qemu-img create -f "${cow_fmt[${i}]}" -o backing_file="${base_img[${i}]}",backing_fmt="${base_fmt[${i}]}" "${cow_img[${i}]}"
+        [[ $? ]] && return $?
+      else
+        echo "[touch_cow[$i]] Found COW image ${cow_img[${i}]}"
+      fi
+    fi
+  done
 }
 
-# $1: nr_cpus
-# $2: memory size
-# $3: unique index (it will be used for serial and macaddr)
-# $4: brname
-# $5: image filename (or the cow filename)
-# $6: image format (or cow format)
-# $7: base filename
-# $8: base format
-boot_one()
+gen_sys()
 {
-  if [[ $# != 6 && $# != 8 ]]; then
-    echo "Usage: $0 <nr-CPUs> <mem-size> <unique-id> <brname> <image> <format> [<backing-image> <backing-format>]"
-    return 1
-  fi
-  if [[ -n "$7" && -n "$8" ]]; then
-    touch_cow "$5" "$6" "$7" "$8"
-    if [[ $? != 0 ]]; then
-      echo "touch_cow failed!"
-      return $?
-    fi
-  fi
-  $(gen_cpu_memory "$1" "$2") $(gen_serial "$3") $(gen_monitor "$3") $(gen_bridge "$4" "$3") $(gen_disk "$5" "$6")
+  echo qemu-system-x86_64 $(gen_display) $(gen_cpu_memory) $(gen_serial) $(gen_monitor) $(gen_bridge) $(gen_disks)
 }
+
+run_sys()
+{
+  touch_cow
+  if [[ $? != 0 ]]; then
+    echo "touch_cow failed!"
+    return $?
+  fi
+  # need special treatment for append. SHIT!
+  if [[ -n "${kernel}" && -n "${initrd}" && -n "${append}" ]]; then
+    $(gen_sys) -kernel "${kernel}" -initrd "${initrd}" -append "${append}"
+  else
+    $(gen_sys)
+  fi
+}
+run_qemu
